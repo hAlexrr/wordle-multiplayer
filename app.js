d45904056016ce47acca8982a30bdf7f29979a54
Alex Rosales <rosalesa512@gmail.com>
2022-05-11 22:25:29 -0700

    Socket.io version implemented when running the game.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const fs = require('fs');
 
+const wordsPath = './words.txt'
 const app = express()
 
 app.use(express.static(`${__dirname}/front_end/`))
@@ -9,8 +11,25 @@ app.use(express.static(`${__dirname}/front_end/`))
 const server = http.createServer(app)
 const io = socketio(server)
 
+const allWords = cleanAllWordsArray(fs.readFileSync(__dirname+'/words.txt', 'utf-8').split('\n'))
+
+let users = {}
+
 io.on('connection', (sock) => {
+    users[sock.id] = {
+        username: '',
+        roomId: generateRandomRoomID(Math.floor(Math.random() * (10-5)+5)),
+        points: 0,
+        word: '',
+        row: 0,
+        pos: 0
+    }
     console.log(sock.id)
+
+    sock.on('disconnect', () => {
+        console.log(`Client Leaving -> ${sock.id}`)
+        delete users[sock.id]
+    })
 
     sock.on('join room', (roomID, cb) => {
             console.log(roomID)
@@ -19,6 +38,40 @@ io.on('connection', (sock) => {
             cb(roomID)
     })
 
+    sock.on('choose word', () => {
+        var word = allWords[getRandomInt(allWords.length)]  
+        console.log(word)
+        users[sock.id].word = word.replace('\r', '')
+    })
+
+    sock.on('isRealWord', (guessedWord, callback) => {
+        if( arrayContains(allWords, guessedWord) )
+            callback(true)
+        callback(false)
+    })
+
+    sock.on('doesWordsMatch', (guessedWord, callback) => {
+        if(guessedWord == users[sock.id].word)
+            callback(true)
+        callback(false)
+    })
+
+    sock.on('getMatchingLetters', (guessedWord, callback) => {
+        var correctLetters = {}
+        var gameWord = users[sock.id].word
+        for( var i = 0; i < guessedWord.length; i++){
+            if(gameWord[i] === guessedWord[i]){
+                correctLetters[i] = 'c'
+            }
+            else if(gameWord.includes(guessedWord[i])){
+                correctLetters[i] = 'p'
+            }
+            else {
+                correctLetters[i] = 'i'
+            }
+        }
+        callback(correctLetters)
+    })
 })
 
 server.on('error', (error) => {
@@ -28,3 +81,28 @@ server.on('error', (error) => {
 server.listen(25352, () => {
     console.log('Server listening on port 25352')
 })
+
+function cleanAllWordsArray(words) {
+    let cleanArray = words
+    for(var i = 0; i < cleanArray.length; i++)
+        cleanArray[i] = cleanArray[i].replace(/(\r\n|\n|\r)/gm,"")
+    return cleanArray
+}
+
+function generateRandomRoomID(len){
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    let id = ''
+
+    for(let x = 0; x < len; x++){
+        id += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return id
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max)
+}
+
+function arrayContains (words, word){
+    return (words.indexOf(word) > -1)
+}
--- front_end/scripts/index.js
+++ front_end/scripts/index.js
@@ -1,146 +1,153 @@
-
 const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 const sock = io();
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
-
     //the event occurred 
-    getWord().then((resolve) => {
-        const board = document.querySelector(".board")
-        const allTiles = document.querySelectorAll('.tile')
-        const allButtons = document.querySelectorAll('button')
-        var row = 0
-        var pos = 0
-        var gameFinished = false
-        var allWords = resolve[0]
-        var gameWord = resolve[1]
-        var prevCorrectLetters = 0
-        var points = 0
+    const board = document.querySelector(".board")
+    const allTiles = document.querySelectorAll('.tile')
+    const allButtons = document.querySelectorAll('button')
+    var row = 0
+    var pos = 0
+    var gameFinished = false
+    var prevCorrectLetters = 0
+    var points = 0
 
-        allButtons.forEach(elem => {
-            elem.addEventListener('click', (e) => {
-                document.dispatchEvent(new KeyboardEvent('keydown', {key: e.target.attributes[0].value}))
-            })
-        })
+    sock.emit('choose word')
 
-        allTiles.forEach(elem => {
-            elem.addEventListener("animationend", function (e){
-                e.target.attributes[2].value = 'Idle'
-                })
-        })
 
-        document.addEventListener('keydown', (e) => {
-            var gameRow = board.children.item(row)
-            var gameItems = gameRow.getElementsByClassName('row')[0].children
-            if(e.key.match(/[a-z]/i) && e.key.length === 1){
-
-                if( gameFinished )
-                    return
-
-                if( pos >= gameItems.length)
-                    return
-
-                pos += 1
-                var gamePos = gameItems.item(pos-1)
-
-                var tile = gamePos.getElementsByClassName('tile')
-
-                tile[0].setAttribute('data-animation', 'PopIn')
-                tile[0].setAttribute('data-state', 'tbd')
-                tile[0].innerHTML = e.key
-                gameRow.setAttribute('letters', gameRow.getAttribute('letters') + e.key )
-            }
-            else if ( e.key === 'Enter' ) {
-                if( gameFinished )
-                    return
-
-                if ( pos !== gameItems.length )
-                    return
-                
-                if ( row >= 6 ) 
-                    return                
-
-                var guessedWord = gameRow.getAttribute('letters')
-                
-                if( !arrayContains(allWords, guessedWord))
-                    return;
-
-                if (gameWord === guessedWord){
-                    console.log('You got the correct answer');
-                    gameFinished = true
-                }
-                
-                var correctLetters = 0
-                var keyboard = document.querySelectorAll('.keys')
-                for( var i = 0; i < guessedWord.length; i++){
-                    var gamePos = gameItems.item(i)
-
-                    var tile = gamePos.getElementsByClassName('tile')
-                    if(gameWord[i] === guessedWord[i]){
-                        correctLetters += 1
-                        tile[0].setAttribute('data-animation', 'flip-out')
-                        tile[0].setAttribute('data-state', 'correct')
-                        keyboard.forEach(elem => {
-                            if ( elem.getAttribute('data-key') === guessedWord[i] ){
-                                elem.setAttribute('data-state', 'correct')
-                                return
-                            }                        
-                        })
-                    }
-                    else if(gameWord.includes(guessedWord[i])){
-                        tile[0].setAttribute('data-animation', 'flip-out')
-                        tile[0].setAttribute('data-state', 'present')
-                        keyboard.forEach(elem => {
-                            if ( elem.getAttribute('data-key') === guessedWord[i] ){
-                                elem.setAttribute('data-state', 'present')
-                                return
-                            }                        
-                        })
-                    }
-                    else {
-                        tile[0].setAttribute('data-animation', 'flip-out')
-                        tile[0].setAttribute('data-state', 'absent')
-                        keyboard.forEach(elem => {
-                            if ( elem.getAttribute('data-key') === guessedWord[i] ){
-                                elem.setAttribute('data-state', 'absent')
-                                return
-                            }                        
-                        })
-                    }
-                }
-                if(correctLetters != prevCorrectLetters){
-                    points += Math.pow(2, correctLetters-prevCorrectLetters)
-                    document.getElementById('Points').innerHTML = `Points: ${points}`
-                    prevCorrectLetters = correctLetters
-                }
-                row += 1
-                if(row >= 6){
-                    gameFinished = true
-                    console.log('Finished game')
-                }
-                pos = 0
-            }
-            else if ( e.key === 'Backspace'){
-                if( gameFinished )
-                    return
-
-                if ( pos <= 0 )
-                    return
-
-                pos -= 1
-                var gamePos = gameItems.item(pos)
-
-                tile = gamePos.getElementsByClassName('tile')
-
-                tile[0].innerHTML = ''
-                tile[0].setAttribute('data-animation', 'PopIn')
-                tile[0].setAttribute('data-state', 'empty')
-                gameRow.setAttribute('letters', gameRow.getAttribute('letters').substring(0, pos) )
-            }
+    allButtons.forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            document.dispatchEvent(new KeyboardEvent('keydown', {key: e.target.attributes[0].value}))
         })
     })
+
+    allTiles.forEach(elem => {
+        elem.addEventListener("animationend", function (e){
+            e.target.attributes[2].value = 'Idle'
+            })
+    })
+
+    document.addEventListener('keydown', (e) => {
+        var gameRow = board.children.item(row)
+        var gameItems = gameRow.getElementsByClassName('row')[0].children
+        if(e.key.match(/[a-z]/i) && e.key.length === 1){
+
+            if( gameFinished )
+                return
+
+            if( pos >= gameItems.length)
+                return
+
+            pos += 1
+            var gamePos = gameItems.item(pos-1)
+
+            var tile = gamePos.getElementsByClassName('tile')
+
+            tile[0].setAttribute('data-animation', 'PopIn')
+            tile[0].setAttribute('data-state', 'tbd')
+            tile[0].innerHTML = e.key
+            gameRow.setAttribute('letters', gameRow.getAttribute('letters') + e.key )
+        }
+        else if ( e.key === 'Enter' ) {
+            if( gameFinished )
+                return
+
+            if ( pos !== gameItems.length )
+                return
+            
+            if ( row >= 6 ) 
+                return                
+            
+            var guessedWord = gameRow.getAttribute('letters')
+
+            sock.emit('isRealWord', guessedWord, function (realWord) {
+                if (!realWord)
+                    return
+
+
+                var correctLetters = 0
+                var keyboard = document.querySelectorAll('.keys')
+
+                sock.emit('doesWordsMatch', guessedWord, function (match) {
+                    if (match){
+                        console.log('match')
+                        gameFinished = true;
+                    }
+
+                    sock.emit('getMatchingLetters', guessedWord, function (letters) {
+                        for( var i = 0; i < guessedWord.length; i++){
+                            console.log(letters[i])
+                            var gamePos = gameItems.item(i)
+            
+                            var tile = gamePos.getElementsByClassName('tile')
+                            if(letters[i] == 'c'){
+                                correctLetters += 1
+                                tile[0].setAttribute('data-animation', 'flip-out')
+                                tile[0].setAttribute('data-state', 'correct')
+                                keyboard.forEach(elem => {
+                                    if ( elem.getAttribute('data-key') === guessedWord[i] ){
+                                        elem.setAttribute('data-state', 'correct')
+                                        return
+                                    }                        
+                                })
+                            }
+                            else if(letters[i] == 'p'){
+                                tile[0].setAttribute('data-animation', 'flip-out')
+                                tile[0].setAttribute('data-state', 'present')
+                                keyboard.forEach(elem => {
+                                    if ( elem.getAttribute('data-key') === guessedWord[i] ){
+                                        elem.setAttribute('data-state', 'present')
+                                        return
+                                    }                        
+                                })
+                            }
+                            else {
+                                tile[0].setAttribute('data-animation', 'flip-out')
+                                tile[0].setAttribute('data-state', 'absent')
+                                keyboard.forEach(elem => {
+                                    if ( elem.getAttribute('data-key') === guessedWord[i] ){
+                                        elem.setAttribute('data-state', 'absent')
+                                        return
+                                    }                        
+                                })
+                            }
+                        }
+                        if(correctLetters != prevCorrectLetters){
+                            points += Math.pow(2, correctLetters-prevCorrectLetters)
+                            document.getElementById('Points').innerHTML = `Points: ${points}`
+                            prevCorrectLetters = correctLetters
+                        }
+                        row += 1
+                        if(row >= 6){
+                            gameFinished = true
+                            console.log('Finished game')
+                        }
+                        pos = 0
+                    })
+                })
+            })
+        }
+        else if ( e.key === 'Backspace'){
+            if( gameFinished )
+                return
+
+            if ( pos <= 0 )
+                return
+
+            pos -= 1
+            var gamePos = gameItems.item(pos)
+
+            tile = gamePos.getElementsByClassName('tile')
+
+            tile[0].innerHTML = ''
+            tile[0].setAttribute('data-animation', 'PopIn')
+            tile[0].setAttribute('data-state', 'empty')
+            gameRow.setAttribute('letters', gameRow.getAttribute('letters').substring(0, pos) )
+        }
+    })
+    
       
 })
 
